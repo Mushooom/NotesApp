e4f8f6be5152ba76a7ae60a47129eb0c5ba36da9
Alexander <62615349+Mushooom@users.noreply.github.com>
2023-10-13 11:59:18 +0100

    Testing issue: NoteAPITest:  - add list all Notes from empty ArrayList  - add list all notes from populated ArrayList  Nested Add notes  Nested List notes Tested-by: Alexander <62615349+mushooom@users.noreply.github.com>

--- src/test/kotlin/controllers/NoteAPITest.kt
+++ src/test/kotlin/controllers/NoteAPITest.kt
@@ -1,10 +1,10 @@
 package controllers
 // Imports
 import models.Note
-//import org.graalvm.compiler.core.common.type.ArithmeticOpTable.UnaryOp.Not
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -48,22 +48,49 @@ class NoteAPITest {
         emptyNotes = null
     }
 
-    @Test  // Test to add new note into ArrayList that is not empty
-    fun `adding a Note fo populated ArrayList`(){
-        val newNote = Note("Study", 1, "College", false)
-        assertEquals(5, populateNotes!!.numberOfNotes())         // 5 notes from dummy data
-        assertTrue(/* condition = */ this.populateNotes!!.add(newNote))  // adding a note
-        assertEquals(6, populateNotes!!.numberOfNotes())         // 6 notes in array list
-        assertEquals(newNote, populateNotes!!.findNote(populateNotes!!.numberOfNotes() -1))
+    @Nested
+    inner class AddNotes {
+
+        @Test  // Test to add new note into ArrayList that is not empty
+        fun `adding a Note fo populated ArrayList`() {
+            val newNote = Note("Study", 1, "College", false)
+            assertEquals(5, populateNotes!!.numberOfNotes())         // 5 notes from dummy data
+            assertTrue(/* condition = */ populateNotes!!.add(newNote))  // adding a note
+            assertEquals(6, populateNotes!!.numberOfNotes())         // 6 notes in array list
+            assertEquals(newNote, populateNotes!!.findNote(populateNotes!!.numberOfNotes() - 1))
+        }
+
+        @Test // Test to add new note into empty ArrayList
+        fun `adding a Note to empty ArrayList`() {
+            val newNote = Note("Lambdas", 2, "College", false)
+            assertEquals(0, actual = emptyNotes!!.numberOfNotes())
+            assertTrue(/* condition = */ emptyNotes!!.add(newNote))
+            assertEquals(1, emptyNotes!!.numberOfNotes())
+            assertEquals(newNote, emptyNotes!!.findNote(index = emptyNotes!!.numberOfNotes() - 1))
+        }
     }
 
-    @Test // Test to add new note into empty ArrayList
-    fun `adding a Note to empty ArrayList`(){
-        val newNote = Note("Lambdas", 2, "College",false)
-        assertEquals(0, actual = emptyNotes!!.numberOfNotes())
-        assertTrue(/* condition = */ this.emptyNotes!!.add(newNote))
-        assertEquals(1, emptyNotes!!.numberOfNotes())
-        assertEquals(newNote, emptyNotes!!.findNote(index = emptyNotes!!.numberOfNotes() -1))
+    @Nested
+    inner class ListNotes {
+
+        // Test to list notes when ArrayListt is empty --> NoteAPI function listAllNotes
+        @Test
+        fun `list all notes for empty ArrayList`() {
+            assertEquals(0, emptyNotes!!.numberOfNotes())
+            assertTrue(emptyNotes!!.listAllNotes().contains("No notes stored"))
+        }
+
+        // Test to list notes for populated ArrayList -- dummy data @BeforeEach
+        @Test
+        fun `list all notes from populated ArrayList`() {
+            assertEquals(5, populateNotes!!.numberOfNotes())
+            val noteString = populateNotes!!.listAllNotes()
+            assertTrue(noteString.contains("Learning Kotlin"))
+            assertTrue(noteString.contains("Summer Holiday"))
+            assertTrue(noteString.contains("Code App"))
+            assertTrue(noteString.contains("Test App"))
+            assertTrue(noteString.contains("Swim"))
+        }
     }
 
 }
